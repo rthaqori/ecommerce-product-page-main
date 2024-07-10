040f812ea22fc8a8f29f9f7475ab63d731f08043
rthaqori <ramanthaqori@gmail.com>
2024-07-10 23:12:36 +0545

    git

--- scripts.js
+++ scripts.js
@@ -29,23 +29,27 @@ function addCart(itemName, quantity, price) {
     saveToStorage();
 }
 
+window.addEventListener("pageshow", ()=>{
+    document.querySelector(".cart-quantity").innerText = cart[0].quantity;
+})
+
 function generateCartHTML() {
     let cartHTML = "";
     cart.forEach(element => {
-        const totalPrice = (cart[0].price * cart[0].quantity).toFixed(2);
+        const totalPrice = (element.price * element.quantity).toFixed(2);
         cartHTML += `
             <div class="item">
               <img src="images/image-product-1.jpg" alt="">
               <div class="cart-item-info">
-                <span id="item-name">${cart.itemName}</span>
+                <span id="item-name">${element.itemName}</span>
                 <div class="price-info">
-                  <span id="item-price">$${cart[0].price}</span>
+                  <span id="item-price">$${element.price}</span>
                   <span>x</span>
-                  <span id="item-quantity">${cart[0].quantity}</span>
+                  <span id="item-quantity">${element.quantity}</span>
                   <span id="total-price">$${totalPrice}</span>
                 </div>
               </div>
-              <button data-item-id="${cart[0].itemName}" id="delete">
+              <button data-item-id="${element.itemName}" id="delete">
                 <img src="images/icon-delete.svg" alt="delete">
               </button>
             </div>
@@ -149,3 +153,86 @@ document.querySelector(".toggle-menu")
             document.querySelector(".nav-list").classList.remove("active");
         });
     });
+
+
+
+    // export function cartProducts() {
+    //     cart.forEach((cartItem) => {
+    
+    //         const productId = cartItem.productId;
+    
+    //         const matchingProduct = products.find(product => product.id === productId);
+    
+    //         const options = [...Array(10).keys()].map(i => {
+    //             const value = i + 1;
+    //             return `<option value="${value}" ${value === cartItem.quantity ? 'selected' : ''}>${value}</option>`;
+    //         }).join('');
+    
+    //         const itemPrice = (matchingProduct.price.sale.dollor + (matchingProduct.price.sale.cents / 100)) * cartItem.quantity;
+    //         totalPrice += itemPrice;
+    
+    //         cartItemsHTML += `
+    //             <div class="cart-item cart-container-${matchingProduct.id}">
+    //                 <div class="checkbox">
+    //                     <input type="checkbox" name="" id="item-checkbox-${matchingProduct.id}">
+    //                 </div>
+    //                 <div class="cart-item-image">
+    //                     <img src="assets/product_images/smartphones/${matchingProduct.image}" alt="${matchingProduct.image}">
+    //                 </div>
+    //                 <div class="cart-item-details">
+    //                     <div class="cart-item-info">
+    //                         <h2><a href="#">${matchingProduct.name}</a>
+    //                         </h2>
+    //                         <span class="stock">${matchingProduct.stock}</span>
+    //                         <label for="gift-${matchingProduct.id}">
+    //                             <input type="checkbox" id="gift-${matchingProduct.id}" class="gift">
+    //                             <span> This is a gift</span>
+    //                             <a href="#" id="lern-more">Lern More</a>
+    //                         </label>
+            
+    //                         <span>
+    //                             Color:
+    //                             <span class="color">${matchingProduct.color}</span>
+    //                         </span>
+    //                         <span>
+    //                             Style:
+    //                             <span class="style">${matchingProduct.style}</span>
+    //                         </span>
+            
+    //                         <div class="item-qty">
+    //                             <span class="item-quentity">
+    //                                 Qty:
+    //                                 <select name="quantity" id="${matchingProduct.id}-quantity" class="quantity" onchange="updateCartItemQuantity(${matchingProduct.id}, this.value)">
+    //                                     ${options}
+    //                                 </select>
+    //                             </span>
+    //                             <a href="#" class="delete-item" data-product-id="${matchingProduct.id}">Delete</a>
+    //                             <a href="#" class="save-item">Save for later</a>
+    //                         </div>
+    //                         <a href="#" class="compare">Compare with similar items</a>
+    //                         <a href="#" class="share">Share</a>
+    //                     </div>
+            
+    //                     <div class="cart-item-price">
+    //                         <span class="item-price">$${itemPrice.toFixed(2)}</span>
+    //                     </div>       
+    //                 </div>
+    //             </div>
+    //             `;
+    
+    //         miniCartHTML += `
+    //                     <div class="cart-item-mini cart-container-${matchingProduct.id}">
+    //                         <div>
+    //                             <img src="assets/product_images/smartphones/${matchingProduct.image}" alt="${matchingProduct.image}">
+    //                         </div>
+    //                         <div class="edit">
+    //                             <select name="quantity" id="${matchingProduct.id}-quantity" class="quantity">
+    //                                 ${options}
+    //                             </select>
+    //                             <button class="delete-item" data-product-id="${matchingProduct.id}"></button>
+    //                         </div>
+    //                     </div>
+    //                 `;
+    
+    //     });
+    // }
